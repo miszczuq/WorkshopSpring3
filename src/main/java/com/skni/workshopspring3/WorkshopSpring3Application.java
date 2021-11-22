69fe9d8d83e0e2663178443d84c1fb1bef13b0b8
Maciej Lis <maciej.lis@softwareplant.com>
2021-11-22 06:57:49 +0100

    Initial commit

---
+++ src/main/java/com/skni/workshopspring3/WorkshopSpring3Application.java
@@ -0,0 +1,13 @@
+package com.skni.workshopspring3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpring3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpring3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/skni/workshopspring3/WorkshopSpring3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skni.workshopspring3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpring3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
