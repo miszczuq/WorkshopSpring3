c199f752af40ea632b8ac6f9d1d53242f953d6f0
miszczuq <michal99miszczuk@gmail.com>
2021-11-27 00:54:56 +0100

    added student and course repositories

--- src/main/java/com/skni/workshopspring3/repository/CourseRepository.java
+++ src/main/java/com/skni/workshopspring3/repository/CourseRepository.java
@@ -1,2 +1,10 @@
-package com.skni.workshopspring3.repository;public class CourseRepository {
+package com.skni.workshopspring3.repository;
+
+import com.skni.workshopspring3.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
 }
--- src/main/java/com/skni/workshopspring3/repository/StudentRepository.java
+++ src/main/java/com/skni/workshopspring3/repository/StudentRepository.java
@@ -1,2 +1,18 @@
-package com.skni.workshopspring3.repository;public interface StudentRepository {
+package com.skni.workshopspring3.repository;
+
+import com.skni.workshopspring3.model.CourseTypeEnum;
+import com.skni.workshopspring3.model.GenderEnum;
+import com.skni.workshopspring3.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+   List<Student> findAll();
+
+   List<Student> findAllByGenderAndCourse_CourseType(GenderEnum gender, CourseTypeEnum course);
+
+   List<Student> findAllByLastname(String lastname);
 }
